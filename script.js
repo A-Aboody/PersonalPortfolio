5e437b01e237d25cecd36672bf3cf5485da1fd08
Abdula Ameen <ameenabdula02@gmail.com>
2024-10-02 13:46:36 -0400

    Image Refinement

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to wrap each letter of a string in a span
 function wrapLetters(selector) {
     const elements = document.querySelectorAll(selector);
     elements.forEach(element => {
@@ -13,25 +12,22 @@ function wrapLetters(selector) {
     });
 }
 
-// Wrap letters for all interactive text elements
 wrapLetters('.interactive-text');
 
-// Scroll down on button click for "Scroll Down" button
 document.getElementById('scroll-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of anchor links
+    event.preventDefault();
     document.getElementById('work-section').scrollIntoView({
         behavior: 'smooth'
     });
 });
 
-// JavaScript for smooth scrolling for navigation links
+
 document.querySelectorAll('.navbar a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevent the default behavior of anchor links
-        const targetId = this.getAttribute('href'); // Get target section ID
-        const targetSection = document.querySelector(targetId); // Select target section
+        e.preventDefault();
+        const targetId = this.getAttribute('href'); 
+        const targetSection = document.querySelector(targetId); 
 
-        // Scroll to target section smoothly
         targetSection.scrollIntoView({
             behavior: 'smooth'
         });
